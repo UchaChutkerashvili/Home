1497cccb50f84c3314aa00badf4b363d05dd0086
Ucha Chutkerashvili <73159581+UchaChutkerashvili@users.noreply.github.com>
2023-05-09 15:02:19 +0400

    Update Homework.js

--- Homework.js
+++ Homework.js
@@ -1,59 +1,31 @@
-// Task 1
-let users = [
-  { name: "Temo", age: 25 },
-  { name: "Lasha", age: 21 },
-  { name: "Ana", age: 28 },
-];
-
-let smallestAge = users[0].age;
-
-function youngestMemberFinder(users) {
-  users.forEach(function (user) {
-    if (user.age < smallestAge) {
-      smallestAge = user.age;
-      console.log(user.name);
+function myFunction(a, b) {
+    if (a === b) {
+        return "ტოლია";
+    } else {
+        return "არ არის ტოლი";
     }
-  });
-}
-youngestMemberFinder(users);
 
-// Task 2
-const user = {
-  firstName: "James",
-  lastName: "Bond",
-  age: 32,
 };
 
- function newObject(user) {
-  
-   let user2 = user
-   return user2
-}
-newObject(user)
-console.log(newObject(user))
+function convertFahrenheitToCelsius(fahrenheit) {
+    if (typeof fahrenheit === 'number') {
+        return (5 / 9 * (celcius - 32));
+    } else {
+        return 'false';
+    }
 
+};
 
-// Task 3
-function randomDice() {
-      return Math.floor((Math.random() * 6) + 1);      
-}
-let resultA = randomDice();
-let resultB = randomDice();
-
-let countA = 0;
-let countB = 0;
-
-while(resultA !==3) {
-  resultA = randomDice()
-  countA++
- 
-}
-while(resultB !==3) {
-  resultB = randomDice()
-  countB++
-}
-if (countA > countB) {
-  console.log('a is a Winner')
-} else {
-  console.log('b is a Winner')
-}
+function Function(a, b, operation) {
+    if (typeof a === 'number' & typeof b === 'number' & operation == '+' ) {
+    return a+b
+    } else if (typeof a === 'number' & typeof b === 'number' & operation == '-') {
+    return a-b
+    } else if (typeof a === 'number' & typeof b === 'number' & operation == '*') {
+    return a*b
+    } else if (typeof a === 'number' & typeof b === 'number' & operation == '/') {
+    return a/b
+    } else if (typeof a !== 'number' || typeof b !== 'number' || operation !== '+') {
+    return false
+    }
+  }
