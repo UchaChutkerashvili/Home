0bc928c68891bc890eb0db266197028f0630ff7f
UchaChutkerashvili <uchutkerashvili@gmail.com>
2023-06-19 23:10:15 +0400

    homework12

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [toDoList, setToDoList] = useState([]);
+  const [doneList, setDoneList] = useState([]);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddToDo = () => {
+    if (inputValue !== '') {
+      const newToDo = {
+        id: Date.now(),
+        text: inputValue
+      };
+
+      setToDoList([...toDoList, newToDo]);
+      setInputValue('');
+    }
+  };
+
+  const handleMarkDone = (id) => {
+    const doneWork = toDoList.find(item => item.id === id);
+    setDoneList([...doneList, doneWork]);
+    setToDoList(toDoList.filter(item => item.id !== id));
+  };
+
+  const handleDeleteToDo = (id) => {
+    setToDoList(toDoList.filter(item => item.id !== id));
+  };
+   const handleDeleteFinishedList = (id) => {
+    setDoneList(doneList.filter(item => item.id !== id));
+  };
+
+  const handleMoveToToDo = (id) => {
+    const toDoWork = doneList.find(item => item.id === id);
+    setToDoList([...toDoList, toDoWork]);
+    setDoneList(doneList.filter(item => item.id !== id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>To Do Tasks</h2>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter a work..."
+        />
+        <button onClick={handleAddToDo}>Add</button>
+        <ul>
+          {toDoList.map(item => (
+            <li key={item.id}>
+              {item.text}
+              <button onClick={() => handleMarkDone(item.id)}>Done</button>
+              <button onClick={() => handleDeleteToDo(item.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>Finished Tasks</h2>
+        <ul>
+          {doneList.map(item => (
+            <li key={item.id}>
+              {item.text}
+              <button onClick={() => handleMoveToToDo(item.id)}>Move to To Do</button>
+              <button onClick={() => handleDeleteFinishedList(item.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
