d9c0580e8a39a00089d1aba33e19a5fffdb7de6f
Ucha Chutkerashvili <73159581+UchaChutkerashvili@users.noreply.github.com>
2023-07-30 11:25:19 +0400

    Add files via upload

--- 22.Redux2/src/App.js
+++ 22.Redux2/src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import './App.dark.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import routes from './router';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { toggleDarkMode } from './store/theme/theme.slice';
+
+
+
+
+
+
 
 function App() {
+  const darkMode = useSelector((state) => state.theme.darkMode);
+  const dispatch = useDispatch();
+  const toggleTheme = () => {
+    dispatch(toggleDarkMode());
+  };
+  console.log(darkMode)
   return (
-    <div className="App">
+    <div className={`App ${darkMode ? 'dark' : 'light'}`}>
+      <button onClick={toggleTheme}>{darkMode ? 'dark' : 'light' }</button>
       <RouterProvider router={createBrowserRouter(routes)} />
+
     </div>
   );
 }
--- 22.Redux2/src/store/index.js
+++ 22.Redux2/src/store/index.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from 'redux';
 import  todoReducer  from "./todo/todo.slice";
 import  doneReducer  from "./done/done.slice";
+import themeReducer from "./theme/theme.slice"
 
 const rootReducer = combineReducers({
     todo: todoReducer,
-        done: doneReducer
+    done: doneReducer,
+    theme: themeReducer
 })
 
 export const store = configureStore({
